e284ddbb50493b956e1a7bd12b11e5f2a222e356
Unknown <Shakugaan01@gmail.com>
2018-08-31 10:47:44 +0200

    Reloading full program from prev. repo
    There was an issue, now is ok?

---
+++ SePr/Employee.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+namespace SePr
+{
+    class Employee
+    {
+        public string id { get; private set; }
+        public int level { get; private set; }
+        public int slave { get; private set; }
+        public double comm { get; private set; }
+        public Employee(string id, int level,int slave, int comm)
+        {
+            this.id = id;
+            this.level = level;
+            this.slave = slave;
+            this.comm = comm;
+        }
+        public Employee(string id, int level, int slave)
+        {
+            this.id = id;
+            this.level = level;
+            this.slave = slave;
+            this.comm = 0;
+        }
+        public void Show()
+        {
+            Console.WriteLine("Id:{0} Level:{1} Slaves:{2} Commission:{3}", id, level, slave, comm);
+        }
+        public void AddComm(double comm)
+        {
+            this.comm += comm;
+        }
+    }
+}
---
+++ SePr/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Xml;
+
+namespace SePr
+{
+    class Program
+    {
+
+
+
+        static void Main(string[] args)
+        {
+            //startup
+            int i = 0;
+            int temp = 0;
+            XmlDocument trans = new XmlDocument();
+            XmlDocument part = new XmlDocument();
+            part.Load("Piramida.xml");
+            XmlNode pir = part.GetElementsByTagName("Piramida").Item(0);
+            trans.Load("Przelewy.xml");
+            XmlNode tran = trans.GetElementsByTagName("Przelewy").Item(0);
+
+            //Couting nodes, Creating the Tab
+            Count(pir, ref i);
+            Employee[] List = new Employee[i];
+
+            //Filling the Tab
+            InsertVal(pir,tran,ref List,ref temp);
+
+            //Result
+            for (int j = 0; j < i; j++)
+                List[j].Show();
+
+            Console.ReadLine();
+        }
+
+        static void InsertVal(XmlNode Pir,XmlNode Tra,ref Employee[] List,ref int temp)
+        {
+            if (!Pir.Name.Equals("Piramida"))
+            {
+                //Level counter
+                int level = 1;
+                if (temp > 0)
+                {
+                   for(int i=0;i<temp;i++)
+                    {
+                        if (List[i].id == Pir.ParentNode.Attributes["id"].Value)
+                        {
+                            level = List[i].level + 1;
+                        }
+                        
+                    }
+                }
+
+                //Filing list with base inf.
+                List[temp] = new Employee(Pir.Attributes["id"].Value, level, Pir.ChildNodes.Count);
+
+                //Counting comm. done by id
+                int X = 0;
+                CountComm(Tra, Pir.Attributes["id"].Value,ref X);
+                //X=100;//Test
+
+                //Filling list with comm.
+                if (X != 0) 
+                {
+                    XmlNode temporary = Pir;
+                    while (temporary.Attributes["id"].Value != "1") 
+                    {
+                        string parid = temporary.ParentNode.Attributes["id"].Value; 
+                        for (int i = 0; i < temp; i++) 
+                        {
+                            if (List[i].id == parid)
+                            {
+                                double A = Math.Pow(2,List[i].level);
+                                if (Pir.ParentNode.Attributes["id"].Value == temporary.ParentNode.Attributes["id"].Value)
+                                { A = A / 2; }
+                                List[i].AddComm(X/A);         
+                            }
+                        }
+                        temporary = temporary.ParentNode;
+                    }
+                }
+
+               //List[temp].Show();//debugDisplay
+               temp++;
+            }
+            foreach (XmlNode n in Pir.ChildNodes)
+            {
+                InsertVal(n,Tra,ref List,ref temp);
+            } 
+        }
+        static void CountComm(XmlNode Tra, string id,ref int val)
+        {
+            if (!Tra.Name.Equals("Przelewy"))
+            {
+                if (Tra.Name.Equals("Przelew") && Tra.Attributes["id"].Value == id)
+                {
+                    foreach(XmlNode n in Tra.ChildNodes)
+                    {
+                        val += int.Parse(n.Attributes["kwota"].Value);
+                    }
+                }
+            }
+            foreach (XmlNode n in Tra.ChildNodes)
+            {
+                CountComm(n,id,ref val);
+            }
+        }
+
+        static void Count(XmlNode node,ref int value)
+        {
+            if (!node.Name.Equals("Piramida"))
+            {
+                value++;
+            }
+            foreach (XmlNode n in node.ChildNodes)
+            {
+                Count(n,ref value);
+            }
+        }
+    }
+}
